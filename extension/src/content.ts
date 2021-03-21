2f827fd4185d8292228477aa51cb0ae24071fe12
Yoshiaki Togami <tuabobo123@gmail.com>
2021-03-21 23:26:25 +0900

    Fixed typo

--- extension/src/content.ts
+++ extension/src/content.ts
@@ -1,4 +1,4 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  document.dispatchEvent(new CustomEvent('csEvent', { detail: request }));
+  document.dispatchEvent(new CustomEvent('customEvent', { detail: request }));
   sendResponse('ok');
 });
