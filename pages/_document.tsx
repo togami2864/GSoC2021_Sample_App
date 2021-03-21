75ad248ff78c9bcd4b2142a9a74a54f6cfd97059
Yoshiaki Togami <tuabobo123@gmail.com>
2021-03-21 21:13:26 +0900

    Resolve style error

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Document, {
   Html,
   Head,
@@ -6,6 +7,7 @@ import Document, {
   DocumentContext,
   DocumentInitialProps,
 } from 'next/document';
+import { ServerStyleSheets } from '@material-ui/core/styles';
 
 class MyDocument extends Document {
   static async getInitialProps(
@@ -67,5 +69,25 @@ class MyDocument extends Document {
     );
   }
 }
+
+MyDocument.getInitialProps = async (ctx) => {
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  };
+};
 
 export default MyDocument;
